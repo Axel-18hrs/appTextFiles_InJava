46454cb39047fb8e674342331928c8340c0eeb80
Axel-18hrs <Axel.18hrs@gmail.com>
2024-02-21 00:55:45 -0600

    Mensaje descriptivo del commit

---
+++ src/main/java/com/mycompany/apptextfiles_java/AppTextFiles_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apptextfiles_java;
+
+/**
+ *
+ * @author axel1
+ */
+public class AppTextFiles_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
